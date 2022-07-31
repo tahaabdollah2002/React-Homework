e99a4f907828a3bbf0f1d0610499623b6758a82e
mohammad taha abdollah <tahaabdollah2002@gmail.com>
2022-08-01 01:11:20 +0430

    countdown timer with start and stop button#12

--- src/Timer.js
+++ src/Timer.js
@@ -9,25 +9,34 @@ class Timer extends React.Component{
             time: 10
         }
     }
-    componentDidMount(){
+    componentDidUpdate(){
+        if(this.state.time == 0){
+            clearInterval(interval)
+        }
+    }
+    stop = () => {
+        clearInterval(interval)
+    }
+    start = () => {
         interval = setInterval(() => {
             this.setState({
                 time: this.state.time - 1
             })
         }, 1000);
     }
-    componentDidUpdate(){
-        if(this.state.time == 0){
-            clearInterval(interval)
-        }
-    }
     render(){
         return(
-            <div className="timer">
-                <h1>
-                    it is {this.state.time}
-                </h1>
-            </div>
+            <React.Fragment>
+                <div className="timer">
+                    <h1>
+                        it is {this.state.time}
+                    </h1>
+                </div>
+                <div>
+                    <button className="btn" onClick={this.start}>Start</button>
+                    <button className="btn" onClick={this.stop}>Stop</button>
+                </div>
+            </React.Fragment>
         )
     }
 }
